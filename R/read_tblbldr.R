271125debd05fe2cef62ed917f6fe3743e4210db
daniel-j-miller <danielmiller364@gmail.com>
2023-07-06 16:32:58 +1000

    refactored

--- R/read_tblbldr.R
+++ R/read_tblbldr.R
@@ -1,27 +1,12 @@
-
-
-
-#this function will read 'csv string' format tables from tablebuilder and Health Workforce data tool
-#detects and skips proceeding and trailing metadata
-#
-#
-#path = location of file to read
-#
-#
-#
-#
-
-
 #' read_tblbldr
 #' Import CVS from TableBuilder
 #' @param path path to the CSV to be imported
 #'
 #' @return a dataframe
 #' @export
-#' @importFrom data.table fread
-#' @importFrom dplyr mutate
-#' @importFrom dplyr where
-#' @importFrom dplyr row_number
+#' @importFrom data.table tstrsplit
+#' @importFrom data.table :=
+#' @importFrom data.table setDT
 #' @importFrom readr read_delim
 #' @importFrom readr read_csv
 #' @importFrom stringr str_count
@@ -31,47 +16,20 @@ read_tblbldr <- function(path){
   df <- readr::read_delim(path,
                           skip_empty_rows = FALSE,
                           delim = "~",
-                          show_col_types = FALSE, )
+                          show_col_types = FALSE)
 
-  df <- dplyr::mutate(df,
-                      i = dplyr::row_number(),
-                      count = stringr::str_count(dplyr::pull(df,1),","))
+  na_pos <- which(is.na(df[,1]))
+  na_diffs <- diff(na_pos)
+  start <- na_pos[which(na_diffs == max(na_diffs))] + 1
+  end <- na_pos[which(na_diffs == max(na_diffs)) + 1] - 1
+  df <- df[start:end,]
 
+  names(df) <- "x"
+  df2 <- df[-1,]
 
-  uniqv <- unique(df$count[!is.na(df$count)])
-
-  m <- uniqv[which.max(tabulate(match(df$count, uniqv)))]
-
-  df <- df[df$count==m,]
-
-  s_min <- min(df$i, na.rm = TRUE)
-
-  s_max <- max(df$i, na.rm = TRUE) - s_min + 1
-
-  h <- head(
-    as.vector(
-      unlist(
-        data.table::fread(path, skip = s_min-1, nrows = 1))), -1)
-
-  if (length(h) < m){
-    h2 <- tail(
-      head(
-        as.vector(
-          unlist(
-            data.table::fread(path, skip = s_min-2, nrows = 1))), -1), -1)
-
-    h <- c(h,h2)
-  }
-
-  r <- readr::read_csv(path,
-                       skip = s_min,
-                       col_names = h,
-                       show_col_types = FALSE,
-                       n_max = s_max, name_repair = "universal")
-
-  r <- dplyr::select(r, dplyr::where(\(x) any(!is.na(x))))
-
-  r
+  setDT(df2)[, gsub(pattern = " ", replacement = ".", strsplit(df$x, ",")[[1]]) := tstrsplit(df2$x, ",", type.convert = TRUE)]
+  df2 <- df2[,-1]
+  df2
 }
 
 
